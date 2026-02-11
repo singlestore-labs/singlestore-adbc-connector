1546f2ecf6ff10c01aea31ada181d11ae29923ac
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2026-02-11 14:41:44 +0200

    Ruff fixes

--- go/validation/tests/singlestore.py
+++ go/validation/tests/singlestore.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from pathlib import Path
 
 from adbc_drivers_validation import model, quirks
-import os
 
 
 class SingleStoreQuirks(model.DriverQuirks):
