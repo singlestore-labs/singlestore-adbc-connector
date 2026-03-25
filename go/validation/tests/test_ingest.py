6fb2e058fe1306162ff3e7d939f24aea89863425
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2026-03-25 14:17:03 +0200

    Linter fixes

--- go/validation/tests/test_ingest.py
+++ go/validation/tests/test_ingest.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 
 import adbc_driver_manager.dbapi
 import adbc_drivers_validation.tests.ingest
